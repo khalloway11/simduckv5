d0685d7c407aa2c8fb425a5f78a9a010bd63fd22
khalloway11 <zacchius@gmail.com>
2014-02-24 19:17:25 -0600

    simduck v5

---
+++ src/version5/MiniDuckSimulator.java
@@ -0,0 +1,20 @@
+package version5;
+
+public class MiniDuckSimulator {
+
+	public static void main(String[] args) {
+		Duck mallard = new MallardDuck();
+		mallard.display();
+		mallard.performQuack();
+		mallard.performFly();
+                mallard.setFlyBehavior(new FlyRocketPowered());
+                mallard.setQuackBehavior(new Squeak());
+
+		Duck model = new ModelDuck();
+		model.display();
+		model.performFly();
+		model.setFlyBehavior( new FlyRocketPowered() );
+		model.performFly();
+	}
+
+}
